088ecf4e9e13492cc0c7f250194c4391c2017495
joeyudongs <yudongs13@gmail.com>
2024-10-26 17:13:30 -0500

    added retrofit and bottom navigation view

--- app/src/main/java/com/dheeraj/composemvvm/MainActivity.kt
+++ app/src/main/java/com/dheeraj/composemvvm/MainActivity.kt
@@ -1,48 +1,48 @@
 package com.dheeraj.composemvvm
 
-import CreditCardScreen
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.dheeraj.composemvvm.ui.theme.ComposeMVVMTheme
+import androidx.appcompat.app.AppCompatActivity
+import com.dheeraj.composemvvm.fragment.KotlinFragment
 import com.dheeraj.composemvvm.viewmodel.CreditCardViewModel
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     private val viewModel: CreditCardViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ComposeMVVMTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    CreditCardScreen(viewModel)
+
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view)
+
+        // Set the initial fragment to KotlinFragment
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, KotlinFragment())
+                .commit()
+        }
+
+        // Listener for BottomNavigationView item selection
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_kotlin -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, KotlinFragment())
+                        .commit()
+                    true
                 }
+                R.id.nav_compose -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, KotlinFragment())
+                        .commit()
+                    true
+                }
+                else -> false
             }
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeMVVMTheme {
-        Greeting("Android")
     }
+
 }
--- app/src/main/java/com/dheeraj/composemvvm/service/CreditCardService.kt
+++ app/src/main/java/com/dheeraj/composemvvm/service/CreditCardService.kt
@@ -5,6 +5,6 @@ import com.dheeraj.composemvvm.model.CreditCard
 import retrofit2.http.GET
 
 interface CreditCardService {
-    @GET("credit_cards")
+    @GET("photos?sol=1000&page=2&api_key=AyHYvkgDh4gCmDpuFPUj5kmc2nLHsJS5ikGGHKHe")
     suspend fun getCreditCards(): CreditCardResponse
 }
