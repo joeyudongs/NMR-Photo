f9a082c0ed9a4e3599c053e80034adff7d9cf3e4
joeyudongs <yudongs13@gmail.com>
2024-10-26 18:24:22 -0500

    updated screen for compose

--- app/src/main/java/com/dheeraj/composemvvm/view/CreditCardScreen.kt
+++ app/src/main/java/com/dheeraj/composemvvm/view/CreditCardScreen.kt
@@ -1,3 +1,5 @@
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -9,10 +11,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.dheeraj.composemvvm.model.Demo
 import com.dheeraj.composemvvm.viewmodel.CreditCardViewModel
-
 
 @Composable
 fun CreditCardScreen(viewModel: CreditCardViewModel) {
@@ -34,33 +41,76 @@ fun CreditCardScreen(viewModel: CreditCardViewModel) {
 }
 
 
+
+@Composable
+fun MarsSection(item: Demo) {
+    Box(
+
+        modifier = Modifier.padding(top = 5.dp)) {
+        AsyncImage(
+            model = item.img_src ,
+            contentDescription = "",
+            modifier = Modifier
+                .requiredHeight(height = 220.dp)
+                .fillMaxWidth(),
+            contentScale = ContentScale.FillHeight
+        )
+        /*Image(
+            painter = painterResource(id = R.mipmap.video_play),
+            "play icon",
+            modifier = Modifier.align(Alignment.Center)
+        )*/
+
+
+        Text(
+            text = item.earth_date ?: "",
+            color = Color.White,
+            modifier = Modifier.align(Alignment.BottomStart)
+        )
+        /*Text(
+            text = item., color = Color.White, modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(end = 8.dp)
+        )*/
+    }
+}
+
 @Composable
 fun CreditCardItem(creditCard: CreditCardResponse) {
     Card(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
+            .fillMaxWidth(),
         elevation = 4.dp
     ) {
-        Column(
-            modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth()
-        ) {
-            Text(
-                text = creditCard.credit_card_number,
-                style = MaterialTheme.typography.h6
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = "Expiry Date: ${creditCard.credit_card_expiry_date}",
-                style = MaterialTheme.typography.body1
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = creditCard.credit_card_type,
-                style = MaterialTheme.typography.body1
-            )
+
+        LazyColumn(modifier = Modifier.fillMaxWidth()) {
+            items(creditCard.photos) {
+                when(it){
+                    is Demo -> MarsSection(item = it)
+                }
+            }
         }
+
+        /*
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth()
+                ) {
+                    Text(
+                        text = "creditCard.photos",
+                        style = MaterialTheme.typography.h6
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        text = "Expiry Date: ",
+                        style = MaterialTheme.typography.body1
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        text = "creditCard.credit_card_type",
+                        style = MaterialTheme.typography.body1
+                    )
+                }*/
     }
 }
